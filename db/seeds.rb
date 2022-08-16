01c3e336514ea400ef686ca64bde9fc45dc9d433
Yann Deret <yann.deretleberre@gmail.com>
2022-08-16 16:43:29 +0200

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Artist.name, content: Faker::DcComics.title)
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
